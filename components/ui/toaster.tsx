0592163b7abf72b103beea25c77583476aa5e94c
Mitya <dim.maluckow2017@yandex.ru>
2025-03-25 15:29:54 +0300

    deleted console.log

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -12,7 +12,6 @@ import {
 
 export function Toaster() {
   const { toasts } = useToast()
-  console.log("Toaster", toasts)
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
