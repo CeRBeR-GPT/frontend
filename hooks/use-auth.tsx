ed98562855f5c444855b8120270c5f90874350ef
Igor Chesnykh <igor.chesnyx@mail.ru>
2025-04-20 14:00:58 +0300

    Start to create plan extension

--- hooks/use-auth.tsx
+++ hooks/use-auth.tsx
@@ -10,6 +10,7 @@ type UserData = {
     id: string,
     email: string,
     plan: string,
+    plan_expire_date: Date,
     available_message_count: number,
     message_length_limit: number,
     message_count_limit: number
