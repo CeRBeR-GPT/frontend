eb785b5f1380cc8846c0c98873556eeeabdac641
Mitya <dim.maluckow2017@yandex.ru>
2025-05-06 20:45:32 +0300

    started create fsd architecture

---
+++ features/auth/ui/ui.tsx
@@ -0,0 +1,12 @@
+// src/features/auth/ui/AuthForm.tsx
+'use client' // Клиентский компонент
+
+export function AuthForm() {
+  //const { login } = useAuth(); // Хук из фичи
+ // return <form onSubmit={login}>...</form>
+}
+
+// src/features/auth/model/useAuth.ts
+export function useAuth() {
+  // Логика авторизации
+}
