10b26abdd5d29c350d7f0088f555ffa71d1eedb3
Mitya <dim.maluckow2017@yandex.ru>
2025-05-11 14:37:59 +0300

    created feature feedback

---
+++ features/feedback/model/api.ts
@@ -0,0 +1,12 @@
+import { apiClient } from '@/shared/api/client';
+
+export const handleSubmitFeedbackApi = async (name: string, message: string, formData: FormData) => {
+    return apiClient.post(`user/feedback?name=${encodeURIComponent(name)}&message=${encodeURIComponent(message)}`, 
+    formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(response => {
+        return response;
+    })
+}
