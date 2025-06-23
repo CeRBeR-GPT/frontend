aca59e00efcffe26fcbf86ee7fbb4729346b7b2e
Mitya <dim.maluckow2017@yandex.ru>
2025-06-23 18:40:13 +0300

    install react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type React from 'react';
 import '@/app/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/shared/providers';
+import { TanstackQueryProvider, ThemeProvider } from '@/shared/providers';
 import { AuthProvider } from '@/shared/hooks';
 import { Toaster } from '@/shared/components/ui/toaster';
 import { MessageProvider } from '@/shared/contexts/MessageContext';
@@ -22,22 +23,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <UserProvider>
-            <AuthProvider>
-              <MessageProvider>
-                <Header />
-                {children}
-              </MessageProvider>
-            </AuthProvider>
-            <Toaster />
-          </UserProvider>
-        </ThemeProvider>
+        <TanstackQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <UserProvider>
+              <AuthProvider>
+                <MessageProvider>
+                  <Header />
+                  {children}
+                </MessageProvider>
+              </AuthProvider>
+              <Toaster />
+            </UserProvider>
+          </ThemeProvider>
+        </TanstackQueryProvider>
       </body>
     </html>
   );
