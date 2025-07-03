83f9e2b616d0f4144875a71252c1e4a75a89d1a3
Mitya <dim.maluckow2017@yandex.ru>
2025-07-03 14:18:44 +0300

    create mutation for login

--- shared/contexts/use-auth.tsx
+++ shared/contexts/use-auth.tsx
@@ -9,6 +9,7 @@ import { useUser } from './user-context';
 import { chatApi } from '@/entities/chat/api';
 import { ApiError } from '@/features/auth/types';
 import { AuthContextType } from '../types/context';
+import { useMutation } from '@tanstack/react-query';
 
 const AuthContext = createContext<AuthContextType>({
   isAuthenticated: false,
@@ -62,44 +63,44 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     }
   }, [isAuthenticated, refreshUserData]);
 
-  const login = async (email: string, password: string) => {
-    try {
+  const { mutateAsync: login } = useMutation({
+    mutationFn: async ({ email, password }: { email: string; password: string }) => {
       const response = await userApi.login(email, password);
 
-      if (response.data?.access_token) {
-        localStorage.setItem('access_token', response.data.access_token);
-        localStorage.setItem('refresh_token', response.data.refresh_token);
-        localStorage.setItem('isAuthenticated', 'true');
-
-        setIsAuthenticated(true);
-        await refreshUserData();
-        setAuthChecked(true);
-
-        const lastSavedChat = localStorage.getItem('lastSavedChat');
-        let welcomeChatId = '1';
-        if (!lastSavedChat) {
-          try {
-            const chatResponse = await chatApi.getAll();
-            if (chatResponse.data) {
-              welcomeChatId = chatResponse.data[0].id;
-              localStorage.setItem('lastSavedChat', chatResponse.data[0].id);
-            }
-          } catch (error) {}
-        }
-
-        return { success: true, lastChatId: lastSavedChat || welcomeChatId };
+      if (!response.data?.access_token) {
+        return { success: false };
       }
-      return { success: false };
-    } catch (error: unknown) {
-      const apiError = error as ApiError;
 
-      if (apiError.response?.status === 401) {
+      localStorage.setItem('access_token', response.data.access_token);
+      localStorage.setItem('refresh_token', response.data.refresh_token);
+      localStorage.setItem('isAuthenticated', 'true');
+
+      setIsAuthenticated(true);
+      await refreshUserData();
+      setAuthChecked(true);
+
+      const lastSavedChat = localStorage.getItem('lastSavedChat');
+      let welcomeChatId = '1';
+      if (!lastSavedChat) {
+        try {
+          const chatResponse = await chatApi.getAll();
+          if (chatResponse.data) {
+            welcomeChatId = chatResponse.data[0].id;
+            localStorage.setItem('lastSavedChat', chatResponse.data[0].id);
+          }
+        } catch (error) {}
+      }
+
+      return { success: true, lastChatId: lastSavedChat || welcomeChatId };
+    },
+    onError: (error: ApiError) => {
+      if (error.response?.status === 401) {
         throw new Error('Неверный логин или пароль. Пожалуйста, попробуйте снова!');
       } else {
         throw new Error('Произошла ошибка при входе. Пожалуйста, попробуйте снова!');
       }
-    }
-  };
+    },
+  });
 
   return (
     <AuthContext.Provider
